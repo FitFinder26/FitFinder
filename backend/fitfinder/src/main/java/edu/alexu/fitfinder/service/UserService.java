dfc1d81788310cbea47c8c1591e8f8aae912e6e0
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2025-10-24 20:03:24 +0300

    Implemented login logic in the backend. [FITFINDER-41]
    * Implemented user signup logic. [FITFINDER-38]
    
    * Implemented login logic in the backend. [FITFINDER-41]
    
    ---------
    
    Co-authored-by: NiraIbrahem <ni254825@gmail.com>

--- backend/fitfinder/src/main/java/edu/alexu/fitfinder/service/UserService.java
+++ backend/fitfinder/src/main/java/edu/alexu/fitfinder/service/UserService.java
@@ -11,6 +11,7 @@ import edu.alexu.fitfinder.service.signup.UserNameValidator;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import edu.alexu.fitfinder.exception.LogInException;
 
 @Service
 public class UserService {
@@ -28,5 +29,22 @@ public class UserService {
 
     String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12));
     userRepo.save(new UserEntity(user.getUserName(), hashedPassword, user.getEmail()));
+  }
+
+  public void LogIn(UserDTO user) throws LogInException {
+    String email = user.getEmail();
+    String password = user.getPassword();
+    if (email == null || password == null) {
+      throw new LogInException("Email and password are required");
+    }
+
+    UserEntity existingUser = userRepo.findByEmail(email);
+    if (existingUser == null) {
+      throw new LogInException("Invalid email or password");
+    }
+
+    if (!BCrypt.checkpw(password, existingUser.getPassword())) {
+      throw new LogInException("Invalid email or password");
+    }
   }
 }
