2e2d90755f1ee084f62dac648e08ca23f53b83fc
alihassann191 <ah2070643@gmail.com>
2025-12-02 20:47:08 +0200

    stored-item-service-and-controller-FITFINDER-139

---
+++ backend/fitfinder/src/main/java/edu/alexu/fitfinder/controller/StoredItemController.java
@@ -0,0 +1,29 @@
+package edu.alexu.fitfinder.controller;
+
+import edu.alexu.fitfinder.entity.StoredItem;
+import edu.alexu.fitfinder.service.StoredItemService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+@CrossOrigin
+public class StoredItemController {
+
+    private final StoredItemService storedItemService;
+
+    public StoredItemController(StoredItemService storedItemService) {
+        this.storedItemService = storedItemService;
+    }
+
+    @PostMapping("/search")
+    public List<StoredItem> getByFaiss(@RequestBody List<Long> faissIds) {
+        return storedItemService.getProductsByFaiss(faissIds);
+    }
+
+    @GetMapping("/{id}")
+    public StoredItem getItemById(@PathVariable Long id) {
+        return storedItemService.getItemById(id);
+    }
+}
