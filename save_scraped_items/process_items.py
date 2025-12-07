cdf5a5b1461414a82cceab4a3e3b184e3962c093
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2025-12-07 17:27:42 +0200

    update process_items.py

--- save_scraped_items/process_items.py
+++ save_scraped_items/process_items.py
@@ -2,6 +2,9 @@ import json
 import glob
 import numpy as np
 import sys
+import httpx
+from PIL import Image
+import io
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -12,10 +15,25 @@ from models.fitfinder_ai_service.app.workers.clip_worker.faiss_manager import (
     add_embedding
 )
 
-
+http_client = httpx.AsyncClient(timeout=30.0)
 
 # TEMP: placeholder embedding generator (until you send the real code)
-def generate_embedding(image_url):
+async def generate_embedding(image_url):
+    try:
+        response = await http_client.get(image_url)
+
+        response.raise_for_status()
+
+        image_bytes = response.content
+        image = Image.open(io.BytesIO(image_bytes))
+
+    except httpx.HTTPStatusError as e:
+        print(f"---  ERROR: HTTP error while downloading: {e} ---")
+        return
+    except httpx.RequestError as e:
+        print(f"---  ERROR: Network error while downloading: {e} ---")
+        return
+
     # TODO: replace with your CLIP embedding code
     # Must return a numpy array of size (512,)
     return np.random.rand(512).astype("float32")
