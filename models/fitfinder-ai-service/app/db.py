20853a4a500db8d177f5c9dec0d794b06d29464f
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2025-12-21 23:44:49 +0200

    Refactor database connection setup: load environment variables and establish connection using psycopg2

--- models/fitfinder-ai-service/app/db.py
+++ models/fitfinder-ai-service/app/db.py
@@ -1,5 +1,26 @@
 from typing import List
-from save_scraped_items.db import get_connection
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()  # loads from .env file
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        sslmode="require",
+    )
 
 
 def get_items_by_faiss_ids(faiss_ids: List[int]):
